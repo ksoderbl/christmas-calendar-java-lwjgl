a417945100dbe749e078411e2ab079df71481157
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2020-09-04 19:06:54 +0300

    Fix run.sh script.

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# To run e.g. example 1_2 do ./run.sh 1_2
+# To run e.g. effect 1 do ./run.sh 1
 
 if [ -n "$1" ]
 then
-	EXAMPLE="$1"
+	EFFECT="$1"
 else
-	EXAMPLE=1_1
+	EFFECT=1
 fi
 
-java -cp .:res:lib/jars/lwjgl.jar:lib/jars/lwjgl_util.jar:lib/jars/slick-util.jar:lib/jars/PNGDecoder.jar:src: -Djava.library.path=lib/natives/  examples/Example_$EXAMPLE
+java -cp .:res:lib/jars/lwjgl.jar:lib/jars/lwjgl_util.jar:lib/jars/slick-util.jar:lib/jars/PNGDecoder.jar:src: -Djava.library.path=lib/natives/  calendar/Effect$EFFECT
